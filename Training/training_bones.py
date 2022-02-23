9d1a3853c6f1103cfb97b33f53dced03e36e09de
Samuel Snider-Held <samuel@mediamonks.com>
2022-02-23 09:07:54 -0500

    Update training_bones.py
    changing reporting functionality to print when loss has gone down

--- Training/training_bones.py
+++ Training/training_bones.py
@@ -17,6 +17,7 @@ def train_bones(args):
         dims=args.img_width, latent_dim = args.latent_dim)
 
     all_history = []
+    lowest_loss = 10000
 
     output_test_csv_dir = os.path.join(args.base_results_dir,args.output_test_csv_dir)
     model_save_path = os.path.join(args.base_results_dir, args.saved_model_dir)
@@ -32,18 +33,19 @@ def train_bones(args):
             shuffle=True,
         )
 
-        if(i%args.print_freq == 0):
-            
-            generate_bone_accuracy_table(bone_auto_encoder,random_sample,random_csv_sample, 
+        loss = round(history.history['loss'][0],4)
+
+        if(loss < lowest_loss):
+             generate_bone_accuracy_table(bone_auto_encoder,random_sample,random_csv_sample, 
                 output_test_csv_dir, i, args.print_csv, args.bone_training)
 
-        if(i%args.save_freq == 0):
-            
             encoder_save_path = os.path.join(model_save_path,f"{i:04d}_encoder_model.h5")
             encoder.save(encoder_save_path)
 
             bone_decoder_save_path = os.path.join(model_save_path,f"{i:04d}_bone_decoder_model.h5")
             bone_decoder.save(bone_decoder_save_path)
+
+            lowest_loss = loss
 
         all_history.append(history.history['loss'][0])
 
